ff965533f6d6e56df81cac107ff040380dfdc446
unknown <udaykiranreddy9908@gmail.com>
2023-10-26 16:41:43 +0530

    first commit

---
+++ login/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework import generics, permissions
+
+from login.serializers import UserLoginSerializer, UserSerializer
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate
+from rest_framework import status
+from django.contrib.auth.models import User
+
+
+
+
+
+# @api_view(['GET', 'POST'])
+
+# def authentication(request):
+#     if request.method=='GET':
+#         return ()
+#     else:
+#         return()
+    
+# class UserRegistrationView(generics.CreateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#     permission_classes = [permissions.AllowAny]
+
+# # views.py
+
+class UserRegistrationView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = User.objects.create_user(
+                username=serializer.data['username'], password=serializer.data['password'], email=serializer.data['email'])
+            user.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+class UserLoginView(APIView):
+    def post(self, request):
+        serializer=UserLoginSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(
+                {'error': 'Bad Request'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        user = authenticate(username=serializer.data['username'], password=serializer.data['password'])
+      
+
+        if user is not None:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        
+
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
